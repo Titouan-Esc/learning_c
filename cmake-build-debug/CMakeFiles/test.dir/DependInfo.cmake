a58f23f239e5e0404cc760f2bff3db99721715e6
Titouan Escorneboueu <titouanescorneboueu@gmail.com>
2023-01-18 08:29:20 +0100

    end of struct and type

--- cmake-build-debug/CMakeFiles/test.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/test.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/titouan/Documents/learning_c/main.c" "CMakeFiles/test.dir/main.c.o" "gcc" "CMakeFiles/test.dir/main.c.o.d"
+  "/Users/titouanescorneboueu/Documents/learning_c/main.c" "CMakeFiles/test.dir/main.c.o" "gcc" "CMakeFiles/test.dir/main.c.o.d"
   )
 
 # Targets to which this target links.
--- main.c
+++ main.c
@@ -4,11 +4,26 @@
 // pointeur->champ = X;
 // Remplace (*pointeur).champ = X;
 
+// Struct
 typedef struct Player {
     signed char username[256];
     int hp;
     int mp;
 } Player;
+
+// Enumération
+typedef enum MarqueVoiture {
+    PEUGEOT, // 0
+    NISSAN, // 1
+    MERCEDES, // 2
+    BMW // 3
+} MarqueVoiture;
+
+// Union
+union Nombre {
+    int entier;
+    float decimal;
+};
 
 void create_player(Player *p) {
     strcpy(p->username, "Diego");
@@ -16,7 +31,7 @@ void create_player(Player *p) {
     p->mp = 100;
 }
 
-int main() {
+int main(void) {
 
     Player p1 = {"", 0, 0};
 
@@ -24,6 +39,13 @@ int main() {
 
     printf("Nom du joueur -> %s\n", p1.username);
     printf("PV : %d | PM : %d\n", p1.hp, p1.mp);
+
+    MarqueVoiture voiture1 = MERCEDES;
+
+    union Nombre nb1;
+
+    nb1.entier = 4;
+    nb1.decimal = 4.8;
 
     return 0;
 }
