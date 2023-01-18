ebf231ddc28968f41ec64502e588619bb82188bb
Titouan Escorneboueu <titouanescorneboueu@gmail.com>
2023-01-18 09:02:12 +0100

    allocation dynamique mémoire

--- main.c
+++ main.c
@@ -1,51 +1,59 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-// pointeur->champ = X;
-// Remplace (*pointeur).champ = X;
-
-// Struct
-typedef struct Player {
-    signed char username[256];
-    int hp;
-    int mp;
-} Player;
-
-// Enumération
-typedef enum MarqueVoiture {
-    PEUGEOT, // 0
-    NISSAN, // 1
-    MERCEDES, // 2
-    BMW // 3
-} MarqueVoiture;
-
-// Union
-union Nombre {
-    int entier;
-    float decimal;
-};
-
-void create_player(Player *p) {
-    strcpy(p->username, "Diego");
-    p->hp = 100;
-    p->mp = 100;
-}
+/*
+ *  sizeof(<donnee>)                        : retourne la taille en octets d'une donnée
+ *  malloc(<taille_en_octets>)              : allouer une taille dynamique en mémoire
+ *  free(<donnee_alloue>)                   : libère la mémoire alloué dynamiquement
+ *  calloc(<donnee>, <taille_en_octets>)    : alloue et initialise tout à 0
+ *  realloc(<donne>, <nouvelle_taille>)     : réalloue un espace mémoire
+*/
 
 int main(void) {
+    int nbPlayer = 0;
+    int *arrayNbPlayer = NULL;
+    int i;
 
-    Player p1 = {"", 0, 0};
+    printf("Combien de joueur ?");
+    scanf("%d", &nbPlayer);
 
-    create_player(&p1);
+//      malloc
+    arrayNbPlayer = malloc(sizeof(int) * nbPlayer);
+//      calloc
+//    arrayNbPlayer = calloc(nbPlayer, sizeof(int));
 
-    printf("Nom du joueur -> %s\n", p1.username);
-    printf("PV : %d | PM : %d\n", p1.hp, p1.mp);
+    if (arrayNbPlayer == NULL)
+        exit(1);
 
-    MarqueVoiture voiture1 = MERCEDES;
+    for (i = 0; i < nbPlayer; i++) {
+        printf("Joueur %d -> numéro %d\n", i + 1, i * 3);
+        arrayNbPlayer[i] = i * 3;
+    }
 
-    union Nombre nb1;
+    for (i = 0; i < nbPlayer; i++) {
+        printf("[%d]\n", arrayNbPlayer[i]);
+    }
+    printf("Adresse de la liste : %p\n", arrayNbPlayer);
+    printf("\n-------------------------------------\n");
 
-    nb1.entier = 4;
-    nb1.decimal = 4.8;
+    nbPlayer = 5;
+
+//    realloc
+    arrayNbPlayer = realloc(arrayNbPlayer, nbPlayer * sizeof(int));
+
+    if (arrayNbPlayer == NULL)
+        exit(1);
+
+    for (i = 0; i < nbPlayer; i++) {
+        printf("Joueur %d -> numéro %d\n", i + 1, i * 3);
+        arrayNbPlayer[i] = i * 3;
+    }
+
+    for (i = 0; i < nbPlayer; i++) {
+        printf("[%d]\n", arrayNbPlayer[i]);
+    }
+    printf("Adresse de la liste : %p\n", arrayNbPlayer);
+    free(arrayNbPlayer);
 
     return 0;
 }
