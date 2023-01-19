35aa6ce02f93b2acaa107b21eac46514cda03cfa
Titouan Escorneboueu <titouanescorneboueu@gmail.com>
2023-01-19 21:35:28 +0100

    file

--- main.c
+++ main.c
@@ -2,58 +2,59 @@
 #include <stdlib.h>
 
 /*
- *  sizeof(<donnee>)                        : retourne la taille en octets d'une donnée
- *  malloc(<taille_en_octets>)              : allouer une taille dynamique en mémoire
- *  free(<donnee_alloue>)                   : libère la mémoire alloué dynamiquement
- *  calloc(<donnee>, <taille_en_octets>)    : alloue et initialise tout à 0
- *  realloc(<donne>, <nouvelle_taille>)     : réalloue un espace mémoire
+ *  fopen(<fichier>, <mode_ouverture>)  : ouvre un fichier
+ *                  - r (lecture seule, doit exister)
+ *                  - w (écriture seule)
+ *                  - a (ajout fin de fichier)
+ *                  - r+ (lecture/écriture, doit exister)
+ *                  - w+ (lecture/écriture, supprime le contenu)
+ *                  - a+ (ajout lecture/écriture, fin de fichier)
+ *
+ *  fclose(<fichier>)   : fermer un fichier ouvert
+ *  feof(<fichier>)      : tester la fin d'un fichier
+ *
+ *  [LECTURE]
+ *  fgetc(<fichier>)                                : lire un caractère
+ *  fgets(<chaine>, <taille_chaine>, <fichier>)     : lit une ligne
+ *  fscanf(<fichier>, <format>, ...)                : lit un texte formaté
+ *
+ *  [ECRITURE]
+ *  fputc(<caractère>, <fichier>)                   : écrit un caractère
+ *  fputs(<chaine>, <fichier>)                      : écrit une chaine de caractère
+ *  fprintf(<fichier>, <format>, ...)               : écrit un texte format
+ *
+ *  [POSITIONNEMENT]
+ *  ftell(<fichier>)                                : retourne une position curseur dans le fichier
+ *  fseek(<fichier>, <deplacement>, <origine>)      : déplace le curseur
+ *                                  - SEEK_SET (début du fichier)
+ *                                  - SEEK_CUR (position courant)
+ *                                  - SEEK_END (fin du fichier)
+ *  rewind(<fichier>                                : réinitialise la position du curseur
 */
 
 int main(void) {
-    int nbPlayer = 0;
-    int *arrayNbPlayer = NULL;
-    int i;
 
-    printf("Combien de joueur ?");
-    scanf("%d", &nbPlayer);
+    FILE *fichier = fopen("/Users/titouanescorneboueu/Documents/learning_c/data.txt", "w+");
+    signed char text[256];
+    char pseudo[] = "Oscar";
+    int age = 22;
+    int year = 2000;
 
-//      malloc
-    arrayNbPlayer = malloc(sizeof(int) * nbPlayer);
-//      calloc
-//    arrayNbPlayer = calloc(nbPlayer, sizeof(int));
-
-    if (arrayNbPlayer == NULL)
+    if (fichier == NULL) {
+        printf("Can't open file\n");
         exit(1);
-
-    for (i = 0; i < nbPlayer; i++) {
-        printf("Joueur %d -> numéro %d\n", i + 1, i * 3);
-        arrayNbPlayer[i] = i * 3;
     }
 
-    for (i = 0; i < nbPlayer; i++) {
-        printf("[%d]\n", arrayNbPlayer[i]);
-    }
-    printf("Adresse de la liste : %p\n", arrayNbPlayer);
-    printf("\n-------------------------------------\n");
+//    Ecriture du fichier
+    fprintf(fichier, "%s %d %d", pseudo, age, year);
 
-    nbPlayer = 5;
+//    Lecture du fichier
+    fscanf(fichier, "%s %d %d", &text, &age, &year);
 
-//    realloc
-    arrayNbPlayer = realloc(arrayNbPlayer, nbPlayer * sizeof(int));
+    printf("Name : %s\n", text);
+    printf("Age : %d\n", age);
+    printf("Year : %d\n", year);
 
-    if (arrayNbPlayer == NULL)
-        exit(1);
-
-    for (i = 0; i < nbPlayer; i++) {
-        printf("Joueur %d -> numéro %d\n", i + 1, i * 3);
-        arrayNbPlayer[i] = i * 3;
-    }
-
-    for (i = 0; i < nbPlayer; i++) {
-        printf("[%d]\n", arrayNbPlayer[i]);
-    }
-    printf("Adresse de la liste : %p\n", arrayNbPlayer);
-    free(arrayNbPlayer);
-
+    fclose(fichier);
     return 0;
 }
